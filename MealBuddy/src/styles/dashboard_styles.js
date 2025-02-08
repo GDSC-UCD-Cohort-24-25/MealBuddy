0f577ea0a9f8051db274b38fbaac1eb9a207e863
Hieu Hoang (Calvin) <127186094+calvinhoang203@users.noreply.github.com>
2025-02-08 12:04:27 -0800

    Create dashboard_styles.js

---
+++ MealBuddy/src/styles/dashboard_styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#212529',
+  },
+  tracker_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+});
